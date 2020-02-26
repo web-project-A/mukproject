ede46de8cd07dab56d9f7a5a846e38ece785cf6b
joshua-mjj <shredakajoshua@gmail.com>
2020-02-26 15:07:30 +0300

    about testing with routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<div>
+    <h1>Welcome to about page</h1>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
