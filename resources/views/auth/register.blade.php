bd1f795ebb527f41fc179fb476b4dfc3c0b357aa
ttcollins <ttamalecollins@gmail.com>
2020-03-02 16:22:32 +0300

    change of login and register looks

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.template')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="row-md-8">
+            <div class="">
+            <span class="login100-form-title p-b-32">
                 <div class="card-header">{{ __('Register') }}</div>
-
+            </span>
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -62,8 +63,8 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="container-login100-form-btn">
+                                <button type="submit" class="login100-form-btn">
                                     {{ __('Register') }}
                                 </button>
                             </div>
